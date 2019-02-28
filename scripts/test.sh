1d33b98e6acb895b45392478943dae92bf8b2fb0
MiaoShi09 <miao@aion.network>
2019-02-28 15:39:13 +0800

    fix the testing env

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,8 @@
 
 node --version
 
-cd ../../chaion_qa
+cd rpc_qa
 file=("smoke-test","AMO","TXTC","FTTC","bugs","precompile")
 type=(http,websocket)
 
-./ci_test_flexible.sh "${file[@]}" "${type[@]}" false
+./ci_test_flexible.sh "${file[@]}" "${type[@]}" true
