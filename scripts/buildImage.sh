2d2a8ac0760b665d5241d0b020dc26da1481f24a
MiaoShi09 <miao@aion.network>
2019-07-12 14:48:30 +0800

    reduce image size

--- scripts/buildImage.sh
+++ scripts/buildImage.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 PACKAGE_NAME=$1
 echo $PACKAGE_NAME
+SYS_LIB="/usr/lib/x86_64-linux-gnu"
+libs=(
+ "libboost_filesystem.so.1.65.1"
+ "libboost_program_options.so.1.65.1"
+ "libboost_regex.so.1.65.1"
+ "libboost_system.so.1.65.1"
+ "libLLVM-4.0.so.1"
+ "libedit.so.2"
+ )
 
 mkdir executive || echo "executive folder exist"
 cp -r ../package/$PACKAGE_NAME executive/$PACKAGE_NAME
+for file in ${libs[@]}
+do
+ echo copy $file
+ if !(test -f $SYS_LIB/$file)
+ then
+   echo Error: $file not found
+   exit 2
+ fi
+ cp $SYS_LIB/$file libs/
+done
 
 docker build --file Dockerfiles/Dockerfile_aionr_0.2 --build-arg PACKAGE_LOCATION=executive/$PACKAGE_NAME -t blockade/aionr .
 
